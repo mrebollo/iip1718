23cda7b2e640d425f5c8b6ab6814abae1bff2f50
M.Rebollo <mrebollo@EVIRL-025-OK.dsic.cloud>
2017-12-12 10:39:45 +0100

    search for a card

--- tema7/Baraja/Carta.java
+++ tema7/Baraja/Carta.java
@@ -12,12 +12,31 @@ public class Carta {
     public static final int TREBOLES = 2;
     public static final int DIAMANTES= 3;
     
+    
     private int palo;
     private int numero;
     
     public Carta(int n, int p){
         numero = n;
         palo = p;
+    }
+    
+    public String toString(){
+        String[] etiqueta = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+        String res = etiqueta[numero] + " de ";;
+        switch(palo){
+            case PICAS: res += "picas"; break;
+            case CORAZONES: res += "corazones"; break;
+            case TREBOLES: res += "tréboles"; break;
+            case DIAMANTES: res += "diamantes"; break;
+        }
+        return res;
+    }
+    
+    public boolean equals(Object o){
+        return o instanceof Carta && 
+            numero == ((Carta)o).numero &&
+            palo == ((Carta)o).palo;
     }
 
 }
--- tema7/Baraja/JuegoCartas.java
+++ tema7/Baraja/JuegoCartas.java
@@ -6,25 +6,18 @@
  * @version (a version number or a date)
  */
 public class JuegoCartas {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class JuegoCartas
-     */
-    public JuegoCartas() {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y
-     */
-    public int sampleMethod(int y) {
-        // put your code here
-        return x + y;
+    public static void main(String[] args){
+       Baraja miBaraja = new Baraja();
+  
+       System.out.println(miBaraja);
+       miBaraja.barajar();
+       System.out.println(miBaraja);
+        
+       Carta asPicas = new Carta(0,Carta.PICAS);
+       
+       Carta cartaEncontrada = miBaraja.buscarCarta(asPicas);
+       
+       int posCarta = miBaraja.buscarPos(asPicas);
+       System.out.println(posCarta);
     }
 }
